baf2c1a72528bd88c985fa920b362e4a695ce3ab
Salief <salieflewis@gmail.com>
2023-09-23 18:10:30 -0400

    add label styling

--- frontend/src/components/client/Mint.tsx
+++ frontend/src/components/client/Mint.tsx
@@ -4,40 +4,44 @@ import {
   useWaitForTransaction,
   useContractRead,
   useAccount,
-} from 'wagmi'
-import { type Hex } from 'viem'
-import { isArtAbi } from '@/abi'
-import { isArtGoerli } from '@/constants'
+} from 'wagmi';
+import { type Hex } from 'viem';
+import { isArtAbi } from '@/abi';
+import { isArtGoerli } from '@/constants';
 
 function IsArt() {
   const { data: getIsArt } = useContractRead({
     address: isArtGoerli,
     abi: isArtAbi,
     functionName: 'getIsArt',
-  })
+  });
 
-  return <>{JSON.stringify(getIsArt)}</>
+  return (
+    <div className='flex rounded-full px-2 bg-slate-100 uppercase text-xs font-medium items-center'>
+      {JSON.stringify(getIsArt)}
+    </div>
+  );
 }
 
 export function Mint() {
-  const { address } = useAccount()
+  const { address } = useAccount();
   const { config, error } = usePrepareContractWrite({
     address: isArtGoerli,
     abi: isArtAbi,
     functionName: 'mint',
-    args: [address as Hex, BigInt(1)],
-  })
+    args: [address as Hex, BigInt(2)],
+  });
 
-  const { data, write: mintArt } = useContractWrite(config)
+  const { data, write: mintArt } = useContractWrite(config);
 
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
-  })
+  });
 
   return (
-    <div className="flex flex-col">
+    <div className='flex gap-2 justify-center'>
       <IsArt />
       <button onClick={() => mintArt?.()}>Mint Art</button>
     </div>
-  )
+  );
 }
