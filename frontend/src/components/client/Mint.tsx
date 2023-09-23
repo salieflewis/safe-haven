810a62764f60fbe23329b8490b0d724a5e735961
Salief <salieflewis@gmail.com>
2023-09-23 13:30:30 -0400

    basic config of is art

--- frontend/src/components/client/Mint.tsx
+++ frontend/src/components/client/Mint.tsx
@@ -2,15 +2,30 @@ import {
   useContractWrite,
   usePrepareContractWrite,
   useWaitForTransaction,
+  useContractRead,
+  useAccount,
 } from 'wagmi';
+import { type Hex } from 'viem';
 import { isArtAbi } from '@/abi';
 import { isArtGoerli } from '@/constants';
 
+function IsArt() {
+  const { data: getIsArt } = useContractRead({
+    address: isArtGoerli,
+    abi: isArtAbi,
+    functionName: 'getIsArt',
+  });
+
+  return <>{JSON.stringify(getIsArt)}</>;
+}
+
 export function Mint() {
-  const { config } = usePrepareContractWrite({
+  const { address } = useAccount();
+  const { config, error } = usePrepareContractWrite({
     address: isArtGoerli,
     abi: isArtAbi,
     functionName: 'mint',
+    args: [address as Hex, BigInt(1)],
   });
 
   const { data, write: mintArt } = useContractWrite(config);
@@ -18,5 +33,11 @@ export function Mint() {
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
   });
-  return <button onClick={() => mintArt?.()}>Mint Art</button>;
+
+  return (
+    <div className='flex flex-col'>
+      <IsArt />
+      <button onClick={() => mintArt?.()}>Mint Art</button>
+    </div>
+  );
 }
